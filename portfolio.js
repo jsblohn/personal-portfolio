66f9fddc667a63794c8c005520ca15e4b4969939
Janet Blohn <jblohn@my365.bellevue.edu>
2020-05-03 23:42:45 -0500

    Add Week 1 Assignments

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+; Title:  portfolio.js
+; Author: Janet Blohn
+; Date:   05/01/2020
+; Modified Date: n/a
+; Description: The Javascript file for the Personal Portfolio Web Page
+;==============================================================================
+*/
+
+var diagramTab;
+var presentTab;
+
+function openDiag() {
+  diagramTab = window
+    .open("web-335/blohn-diagrams.html", "_blank")
+ }
+
+function closeDiag() {
+  window.close()
+}
+
+function openPres() {
+  presentTab = window
+    .open("web-420/blohn-rest.html", "_blank")
+ }
+
+function closePres() {
+  window.close()
+}
